3ffec195e1836b61ab75ca001316fd5a908a79d0
nonl4331 <nicholasl0003@gmail.com>
2024-10-16 11:03:26 +1100

    Changed to motor control example

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,33 @@
-use plotly::Histogram;
-use plotly::Plot;
 use robot_serial::protocol;
 
 fn main() {
     env_logger::init();
-
-    const TEST_SIZE: usize = 485;
-    let pkt = protocol::ToBrain::Ping(vec![0; TEST_SIZE]);
-    let vec = postcard::to_stdvec_cobs(&pkt).unwrap();
-    log::info!(
-        "encoded test packet size: {} from size {TEST_SIZE}",
-        vec.len()
-    );
-
-    log::info!("waiting for brain connection");
-    let mut bm;
-    loop {
-        if let Some(v) = robot_serial::BrainMediator::new() {
-            bm = v;
-            log::info!("brain connected");
-            break;
-        }
-    }
-
-    let mut sent = false;
-    let mut now = std::time::Instant::now();
-    let mut samples = Vec::new();
+    let mut bm = robot_serial::BrainMediator::new().unwrap();
+    let mut motor_power = 0.0;
 
     loop {
-        if samples.len() > 1000 {
-            break;
-        }
-        if !sent {
-            if let Err(e) = bm.try_write(&pkt) {
-                log::warn!("{e:?}");
-            } else {
-                sent = true;
-                now = std::time::Instant::now();
-            }
-        }
-        match bm.try_read() {
-            Ok(v) => {
-                for e in &v {
-                    match e {
-                        protocol::ToRobot::Pong(_) => {
-                            samples.push(now.elapsed().as_micros());
-                            log::info!(
-                                "round trip of size {TEST_SIZE} (encoded size {}) done in: {}ms",
-                                vec.len(),
-                                now.elapsed().as_millis()
-                            );
-                            sent = false;
-                        }
-                        v => log::info!("got other: {v:?}"),
+        let _ = bm.try_write(&protocol::ToBrain::RequestControllerInfo);
+        let _ = bm.try_write(&protocol::ToBrain::RequestDeviceInfo);
+        if let Ok(pkts) = bm.try_read() {
+            for pkt in pkts {
+                match pkt {
+                    protocol::ToRobot::ControllerState(controller_state) => {
+                        motor_power = controller_state.axis[0];
                     }
+                    protocol::ToRobot::DevicesList(devices_list) => {
+                        let mut arr = [protocol::MotorControl::default(); 21];
+                        for (i, device) in devices_list.smart_ports.iter().enumerate() {
+                            if protocol::PortState::Motor == *device {
+                                arr[i] = protocol::MotorControl::Voltage(
+                                    12.0 * (i as f64 + 1.0) / 21.0 * motor_power,
+                                );
+                            }
+                        }
+                        let _ = bm.try_write(&protocol::ToBrain::SetMotors(arr));
+                    }
+                    _ => {}
                 }
             }
-            Err(e) => log::warn!("{e:?}"),
         }
     }
-    let mut plot = Plot::new();
-    let trace = Histogram::new(samples).name(format!(
-        "round trip time with an encoded packet size of {}",
-        vec.len()
-    ));
-    plot.add_trace(trace);
-    plot.show();
 }
