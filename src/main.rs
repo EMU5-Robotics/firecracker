cd5dec12e5ec4d21ba79dd712d4f117a0286029e
nonl4331 <nicholasl0003@gmail.com>
2024-11-06 13:48:03 +1100

    Moved to megapacket approach

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,43 @@ fn main() {
     env_logger::init();
     let mut bm = robot_serial::BrainMediator::new().unwrap();
     let mut motor_power = 0.0;
-
+    /*    let _ = bm
+        .try_write(&protocol::ToBrain::RequestControllerInfo)
+        .unwrap();
+    let _ = bm.try_write(&protocol::ToBrain::RequestDeviceInfo).unwrap();*/
+    let mut write_brain = protocol::ToBrain::default();
+    let mut brain_state = protocol::ToRobot::default();
     loop {
-        let _ = bm.try_write(&protocol::ToBrain::RequestControllerInfo);
-        let _ = bm.try_write(&protocol::ToBrain::RequestDeviceInfo);
         if let Ok(pkts) = bm.try_read() {
             for pkt in pkts {
+                log::info!("{pkt:?}");
+                brain_state = pkt;
+            }
+        } else {
+            log::warn!("AAAAA");
+        }
+
+        if let Some(ref controller) = brain_state.controller_state {
+            motor_power = controller.axis[0];
+        }
+
+        if let Some(ref device_list) = brain_state.device_list {
+            for (i, device) in device_list.smart_ports.iter().enumerate() {
+                if protocol::PortState::Motor == *device {
+                    write_brain.set_motors[i] = protocol::MotorControl::Voltage(
+                        12.0 * (i as f64 + 1.0) / 21.0 * motor_power,
+                    );
+                }
+            }
+        }
+
+        bm.try_write(&write_brain).unwrap();
+    }
+
+    /*loop {
+        if let Ok(pkts) = bm.try_read() {
+            for pkt in pkts {
+                println!("AAAAA");
                 match pkt {
                     protocol::ToRobot::ControllerState(controller_state) => {
                         motor_power = controller_state.axis[0];
@@ -28,6 +59,8 @@ fn main() {
                     _ => {}
                 }
             }
+        } else {
+            println!("FASFASFASF");
         }
-    }
+    }*/
 }
